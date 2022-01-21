2e3109c7a59dd432e4b1bbe36db99abffb2c1377
Guiorgy <Guiorgy123@gmail.com>
2022-01-21 17:12:08 +0400

    possible solution to #16

--- vaksina/validators.py
+++ vaksina/validators.py
@@ -34,21 +34,32 @@ def is_immunization_older_than_14_days(immunization):
 
 
 class ValidationResult(object):
-    def __init__(self):
+    def __init__(self, validation_method, person):
+        self.validation_method = validation_method
+        self.person = person
         self.card_validation_status = "invalid"
         self.validation_errors = None
 
     def to_dict(self):
-        vr_dict = {}
-        vr_dict["card_validation_status"] = self.card_validation_status
-        if self.validation_errors is not None:
-            vr_dict["validation_errors"] = self.validation_errors
+        vr_dict = {
+            "validation_method": self.validation_method,
+            "results": {
+                self.person.names[0]
+                if len(self.person.names) == 1 else
+                "[ " + '; '.join(self.person.names) + " ]":
+                    self.card_validation_status
+                    if self.card_validation_status != "failed" or self.validation_errors is None
+                    else self.validation_errors
+            }
+        }
         return vr_dict
 
 
 class Validators(object):
-    def __init__(self, v):
+    def __init__(self, v, cardset):
         self.v_obj = v
+        self.cardset = cardset
+        self.validations = []
 
     def validator_osha_1910_501_rules(self, person):
         """
@@ -73,7 +84,7 @@ class Validators(object):
         one_shot_vaccines = vacinfo.get_vaccines_by_required_doses(1)
         two_shot_vaccines = vacinfo.get_vaccines_by_required_doses(2)
 
-        result = ValidationResult()
+        result = ValidationResult("osha_1910_501", person)
 
         # Get immunizations for person
         immunizations = person.immunizations
@@ -84,6 +95,7 @@ class Validators(object):
                 if immunization.is_vaccine(vaccine):
                     if is_immunization_older_than_14_days(immunization):
                         result.card_validation_status = "success"
+                        self.validations.append(result)
                         return
 
         # So validate the two shot test cases now ...
@@ -121,7 +133,16 @@ class Validators(object):
                 # is the duty of the healthcare provider
                 if is_immunization_older_than_14_days(newest_vaccination):
                     result.card_validation_status = "success"
+                    self.validations.append(result)
                     return result
 
         result.card_validation_status = "failed"
+        self.validations.append(result)
         return result
+
+    def to_dict(self):
+        v_dict = {
+            "cards": self.cardset.to_dict(),
+            "validations": [val.to_dict() for val in self.validations]
+        }
+        return v_dict
